0c1e5a3b57e121c9e606a00e91bd657361d473a5
Fatema Damani <fatemadayamani@fatemas-mbp.home>
2020-04-16 21:52:19 +0100

    Fixed bug where it was randomly selecting any ayah, not just the start of a ruku

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,8 +4,11 @@ import static java.lang.Integer.parseInt;
 
 public class App {
     public static void main (String[] args) {
-        List<Ayah> ayaat = Utils.readData("src/main/resources/ayaat.txt");
-        Ayah startingVerse = Utils.selectRandomStartPoint(ayaat);
+        if (args.length != 1) {
+            throw new RuntimeException("Please enter how many verses you wish to recite");
+        }
+        List<Ayah> ayaat = Utils.readData("src/main/resources/ayaat.csv");
+        Ayah startingVerse = Utils.selectRandomStartPoint(Utils.findRuku(ayaat));
         AyahPicker ayahPicker = new AyahPicker(ayaat);
         System.out.println(ayahPicker.pickVerses(parseInt(args[0]), startingVerse));
     }
