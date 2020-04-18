bd7e9e5dfa6e00d0fb2158d54f87e34044ffa103
Fatema Damani <fatema.damani@gmail.com>
2020-04-18 14:33:15 +0100

    Adds exercise 1 - to do with data types and creating objects

---
+++ src/main/java/practice/ExerciseOne.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class ExerciseOne {
+    //id|	surah name|	ayah in surah|	start of ruku
+    //0 |	Al-Fatihah|	            1|	true
+    public static void main(String[] args) {
+        //Exercise 1: create a "Verse" object which will contain the 4 pieces of data above
+        //Uncomment the line below. There should be no red lines!
+
+        //new Verse(0, "Al-Fatihah", 1, true);
+    }
+}
