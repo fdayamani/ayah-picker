66757a9abe4cd049dab6f997ab827f471b0ca1de
Fatema Damani <fatema.damani@gmail.com>
2020-04-18 15:54:28 +0100

    Adds second exercise (to pick a random passage without considering ruku)

--- src/main/java/practice/ExerciseTwo.java
+++ src/main/java/practice/ExerciseTwo.java
@@ -1,7 +1,9 @@
-package practice.exercise2;
+package practice;
 
 public class ExerciseTwo {
     public static void main (String[] args) {
-
+        String path = "src/main/resources/ayaat.csv";
+//        List<Ayah> ayaat = Utils.readData(path);
+//        Ayah ayah = Utils.selectRandomStartPoint(ayaat);
     }
 }
