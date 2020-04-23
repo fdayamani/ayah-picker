e7b99d708f756f9e2a562f22e6d6e99b82538459
Fatema Damani <fatema.damani@gmail.com>
2020-04-23 16:24:36 +0100

    Removes args checker now that we're using a dialogue box instead

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -10,9 +10,6 @@ public class App {
     public static void main (String[] args) {
         int numberOfAyaatToRead = parseInt(showInputDialog("How many ayaat do you want to read at the moment?"));
 
-        if (args.length != 1) {
-            throw new RuntimeException("Please enter how many verses you wish to recite");
-        }
         List<Ayah> ayaat = readData("src/main/resources/ayaat.csv");
         Ayah startingVerse = selectRandomStartPoint(findRuku(ayaat));
         AyahPicker ayahPicker = new AyahPicker(ayaat);
