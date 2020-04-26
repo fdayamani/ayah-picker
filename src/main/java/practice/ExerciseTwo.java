ced98d1aa64e91d7c26bd439823ab4ac6582efd4
Fatema Damani <fatema.damani@gmail.com>
2020-04-26 11:54:28 +0100

    Adds sandbox area and adds some details to exercise two

--- src/main/java/practice/ExerciseTwo.java
+++ src/main/java/practice/ExerciseTwo.java
@@ -2,10 +2,17 @@ package practice;
 
 public class ExerciseTwo {
     public static void main (String[] args) {
+        /*Exercise Two: Given a random starting ayah in the Holy Qur'an,
+        figure out what the end ayah would be if you wanted to read 20 verses.
+        Uncomment the lines below and fill in the gaps!*/
+
         String path = "src/main/resources/ayaat.csv";
 //        List<Ayah> ayaat = Utils.readData(path);
 //        Ayah startingAyah = Utils.selectRandomStartPoint(ayaat);
-//        Ayah endAyah = ;
-//        System.out.println("Read from " + startingAyah.surah + ":" + startingAyah.ayahNumber + " to " + endAyah.surah + ":" + endAyah.ayahNumber);
+//
+//        Ayah endAyah = ; // Change this line
+//
+//        System.out.println("Read from " + startingAyah.surah + ":" + startingAyah.ayahNumber +
+//                " to " + endAyah.surah + ":" + endAyah.ayahNumber);
     }
 }
---
+++ src/main/java/practice/ExerciseZero.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class ExerciseZero {
+    public static void main (String[] args) {
+        //This is an area you can write any code you want and try things out
+        System.out.println("Hello");
+
+    }
+}
