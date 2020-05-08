85a061582006b991afa3b6f53d94f60f870af9eb
Fatema Damani <fatema.damani@gmail.com>
2020-05-08 16:14:07 +0100

    Lays out the exercise in a nicer format

--- src/main/java/practice/ExerciseTwo.java
+++ src/main/java/practice/ExerciseTwo.java
@@ -1,18 +1,27 @@
 package practice;
 
+import java.util.List;
+
 public class ExerciseTwo {
     public static void main (String[] args) {
         /*Exercise Two: Given a random starting ayah in the Holy Qur'an,
-        figure out what the end ayah would be if you wanted to read 20 verses.
-        Uncomment the lines below and fill in the gaps!*/
-
+        figure out what the end ayah would be if you wanted to read 20 verses.*/
         String path = "src/main/resources/ayaat.csv";
-//        List<Ayah> ayaat = Utils.readData(path);
-//        Ayah startingAyah = Utils.selectRandomStartPoint(ayaat);
+
+//        List<Ayah> ayaat;
+//        ayaat = Utils.readData(path);
 //
-//        Ayah endAyah = ; // Change this line
+//        Ayah startingAyah;
+//        startingAyah = Utils.selectRandomStartPoint(ayaat);
 //
-//        System.out.println("Read from " + startingAyah.surah + ":" + startingAyah.ayahNumber +
-//                " to " + endAyah.surah + ":" + endAyah.ayahNumber);
+//        int idOfStartingAyah;
+//
+//        int idOfEndingAyah;
+//
+//        Ayah endAyah;
+
+
+//        System.out.println("Read from " + startingAyah.surahName + ":" + startingAyah.ayahInSurah +
+//                " to " + endAyah.surahName + ":" + endAyah.ayahInSurah);
     }
 }
