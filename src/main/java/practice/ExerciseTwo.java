221dc461192c7255f53754c01ef9a86b8954b3f0
Fatema Damani <fatema.damani@gmail.com>
2020-05-22 14:45:23 +0100

    Comment out ExerciseTwo

--- src/main/java/practice/ExerciseTwo.java
+++ src/main/java/practice/ExerciseTwo.java
@@ -7,18 +7,18 @@ public class ExerciseTwo {
         /*Exercise Two: Given a random starting ayah in the Holy Qur'an,
         figure out what the end ayah would be if you wanted to read 20 verses.*/
         String path = "src/main/resources/ayaat.csv";
-
-        List<Ayah> ayaat;
-        ayaat = Utils.readData(path);
-
-        Ayah startingAyah;
-        startingAyah = Utils.selectRandomStartPoint(ayaat);
-
-        int idOfStartingAyah;
-
-        int idOfEndingAyah;
-
-        Ayah endAyah;
+//
+//        List<Ayah> ayaat;
+//        ayaat = Utils.readData(path);
+//
+//        Ayah startingAyah;
+//        startingAyah = Utils.selectRandomStartPoint(ayaat);
+//
+//        int idOfStartingAyah;
+//
+//        int idOfEndingAyah;
+//
+//        Ayah endAyah;
 
 
 //        System.out.println("Read from " + startingAyah.surahName + ":" + startingAyah.ayahInSurah +
