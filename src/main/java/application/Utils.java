80f70638cb2f38900f6adc8b8d96ec80051a5e8c
Fatema Damani <fatema.damani@gmail.com>
2020-05-22 15:13:21 +0100

    Specifies main class name to be able to run from command line

--- src/main/java/application/Utils.java
+++ src/main/java/application/Utils.java
@@ -5,7 +5,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static java.lang.Boolean.parseBoolean;
 import static java.lang.Integer.parseInt;
