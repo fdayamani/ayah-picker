d9e884d0b3c3aff0e6e77f3dcd4dbc4db75bdb10
Fatema Damani <fatema.damani@gmail.com>
2020-06-16 14:24:15 +0100

    Corrections to ex one

--- src/main/java/practice/ExerciseOne.java
+++ src/main/java/practice/ExerciseOne.java
@@ -2,14 +2,14 @@ package practice;
 
 public class ExerciseOne {
 
-/*    id|	surahName|	ayahInSurah|	startOfRuku
-      0 |	Al-Fatihah|	            1|	true            */
+/*    id|	surahName |	ayahInSurah| startOfRuku
+      0 |	Al-Fatihah|	          1| true            */
 
     public static void main(String[] args) {
-/*      Exercise 1: create a "Verse" object which will contain the 4 pieces of data above
+/*      Exercise 1: create an "Ayah" object which will contain the 4 pieces of data above
         Uncomment the lines below. There should be no red lines!        */
 
-//        new Verse(0, "Al-Fatihah", 1, true);
-//        new Verse(1, "Al-Fatihah", 2, false);
+//        new Ayah(0, "Al-Fatihah", 1, true);
+//        new Ayah(1, "Al-Fatihah", 2, false);
     }
 }
